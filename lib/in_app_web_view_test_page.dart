58792ffb7be6d0842dd7333745f1eba052162cf9
minseok-gn <minseok.jeong@genesisnest.com>
2025-04-15 16:47:06 +0900

    feat: load local test server url for check flutter javascript code

--- lib/in_app_web_view_test_page.dart
+++ lib/in_app_web_view_test_page.dart
@@ -1,6 +1,10 @@
-import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 WebViewEnvironment? webViewEnvironment;
@@ -14,70 +18,44 @@ class InAppWebViewTestPage extends StatefulWidget {
 
 class _InAppWebViewTestPageState extends State<InAppWebViewTestPage> {
   InAppWebViewSettings settings = InAppWebViewSettings(
-      isInspectable: kDebugMode,
-      mediaPlaybackRequiresUserGesture: false,
-      allowsInlineMediaPlayback: true,
-      iframeAllow: "camera; microphone",
-      iframeAllowFullscreen: true);
+    isInspectable: kDebugMode,
+    mediaPlaybackRequiresUserGesture: false,
+    allowsInlineMediaPlayback: true,
+    iframeAllow: "camera; microphone",
+    iframeAllowFullscreen: true,
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("JavaScript Handlers")),
+          appBar: AppBar(title: const Text("JavaScript Handlers")),
           body: SafeArea(
               child: Column(children: <Widget>[
             Expanded(
               child: InAppWebView(
                 webViewEnvironment: webViewEnvironment,
-                initialData: InAppWebViewInitialData(data: """
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    </head>
-    <body>
-        <h1>JavaScript Handlers</h1>
-        <script>
-            window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
-                window.flutter_inappwebview.callHandler('handlerFoo')
-                  .then(function(result) {
-                    // print to the console the data coming
-                    // from the Flutter side.
-                    console.log(JSON.stringify(result));
-
-                    window.flutter_inappwebview
-                      .callHandler('handlerFooWithArgs', 1, true, ['bar', 5], {foo: 'baz'}, result);
-                });
-            });
-        </script>
-    </body>
-</html>
-                      """),
                 initialSettings: settings,
-                onWebViewCreated: (controller) {
-                  controller.addJavaScriptHandler(
-                      handlerName: 'handlerFoo',
-                      callback: (params) {
-                        // return data to the JavaScript side!
-                        return {'bar': 'bar_value', 'baz': 'baz_value'};
-                      });
+                onWebViewCreated: (controller) async {
+                  await controller.platform.clearAllCache();
 
                   controller.addJavaScriptHandler(
-                      handlerName: 'handlerFooWithArgs',
-                      callback: (params) {
-                        print('params: ${params.runtimeType}');
-                        for (final param in params) {
-                          print('${param.runtimeType}: $params');
-                        }
-                        // it will print:
-                        // JavaScriptHandlerFunctionData{args: [1, true, [bar, 5], {foo: baz}, {bar: bar_value, baz: baz_value}], isMainFrame: true, origin: , requestUrl: about:blank}
-                      });
+                    handlerName: 'getDeviceInfo',
+                    callback: (_) async {
+                      late final BaseDeviceInfo deviceInfo;
+                      if (Platform.isAndroid) {
+                        deviceInfo = await DeviceInfoPlugin().androidInfo;
+                      } else {
+                        deviceInfo = await DeviceInfoPlugin().iosInfo;
+                      }
+                      return deviceInfo.data;
+                    },
+                  );
+
+                  controller.loadUrl(urlRequest: URLRequest(url: WebUri('http://192.168.50.147:8080/index.html')));
                 },
                 onConsoleMessage: (controller, consoleMessage) {
-                  print(consoleMessage);
-                  // it will print: {message: {"bar":"bar_value","baz":"baz_value"}, messageLevel: 1}
+                  log(consoleMessage.toString());
                 },
               ),
             ),
