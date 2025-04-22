716515aa5778bb7b6ffbbf5c415d65e9cdaf8a8b
minseok-gn <minseok.jeong@genesisnest.com>
2025-04-22 14:07:13 +0900

    feat: page change duration change more shorter

--- lib/long_duration_transition_page_route.dart
+++ lib/long_duration_transition_page_route.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 class LongDurationTransitionPageRoute extends MaterialPageRoute {
   LongDurationTransitionPageRoute({required super.builder});
 
-  final _transitionDuration = Durations.long4;
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
 
   @override
-  Duration get transitionDuration => _transitionDuration;
-
-  @override
-  Duration get reverseTransitionDuration => _transitionDuration;
+  Duration get reverseTransitionDuration => const Duration(milliseconds: 300);
 }
